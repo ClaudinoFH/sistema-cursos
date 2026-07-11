b8e3795720ca93208b198e9efb724ae1dfb6ae94
ClaudinoFH <claudinofilho11@gmail.com>
2026-07-11 18:30:47 -0300

    Design: adicionando mapa interativo do Google Maps

--- app/page.tsx
+++ app/page.tsx
@@ -54,12 +54,12 @@ export default function Home() {
             </div>
             <div className="w-full h-40 rounded-xl overflow-hidden border border-zinc-200 shadow-sm mt-3">
               <iframe
-                src="https://share.google/gdz7LNuH6K7NsVe0K"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3974.2608258818514!2d-42.78144412501843!3d-5.061416994915312!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x78e3b61491cbd21%3A0x18011bdad6e5c977!2sJosefa%20Poty%20Fisioterapia%20Neuroinfantil!5e0!3m2!1spt-BR!2sbr!4v1783805153732!5m2!1spt-BR!2sbr"
                 className="w-full h-full border-0"
                 allowFullScreen={true}
                 loading="lazy"
                 referrerPolicy="no-referrer-when-downgrade"
-              />
+              ></iframe>
             </div>
           </div>
 
